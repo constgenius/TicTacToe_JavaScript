b9c74999b5aafd03e261e4b7618f3884ad6d0f38
robin8778 <robin.007660@gmail.com>
2023-08-06 00:33:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const X_CLASS='x'
+const CIRCLE_CLASS='circle'
+const cellElements= document.querySelectorAll('[data-cell]')
+const board=document.getElementById("board")
+const winningMessageTextElement=document.querySelector('[data-winning-message-text]')
+const winningMessageElement=document.getElementById('winning-message')
+const restartButton=document.getElementById('restartButton')
+const WINNING_COMBINATIONS=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+
+let circleTurn
+
+startGame()
+restartButton.addEventListener('click',startGame)
+
+function startGame(){
+    circleTurn=false
+    cellElements.forEach(cell=>{
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click',handleClick)
+        cell.addEventListener('click',handleClick,{once:true})
+    })
+    setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
+}
+
+
+
+function handleClick(e){
+    const cell=e.target;
+    const currentClass=circleTurn?CIRCLE_CLASS:X_CLASS
+
+    //placeMark
+    placeMark(cell,currentClass)
+    //Check For Win
+    if(checkWin(currentClass)){
+        endGame(false)
+    }else if(isDraw()){
+        endGame(true)
+    }else{
+    //Check For Draw
+    //Switch Turn
+    swapTurns()
+    setBoardHoverClass()
+    }
+    
+}
+
+function endGame(draw){
+    if(draw){
+        winningMessageTextElement.innerText="Draw!"
+    }else{
+        winningMessageTextElement.innerText=`${circleTurn?"O's":"X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function isDraw(){
+    return [...cellElements].every(cell=>{
+        return cell.classList.contains(X_CLASS)|| cell.classList.contains(CIRCLE_CLASS)
+    })
+}
+
+function placeMark(cell,currentClass){
+    cell.classList.add(currentClass)
+}
+
+function swapTurns(){
+    circleTurn=!circleTurn
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if(circleTurn){
+        board.classList.add(CIRCLE_CLASS)
+    }else{
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination=>{
+        return combination.every(index=>{
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
